10e3ee65e4126880736f6a43ef1b3a6ed8d9d743
Amruta Jahagirdar <ajahagirdar8@gmail.com>
2020-08-24 07:22:58 +0530

    initial commit

---
+++ QuickUnion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace QuickUnion
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter size of array");
+
+			string s = Console.ReadLine ();
+
+			int N = int.Parse (s);
+
+			QuickUnion qu = new QuickUnion (N);
+			bool isConected = qu.isConnected (2, 3);
+			Console.WriteLine ("2 and 3 " + isConected);
+
+			qu.Union (2, 3);
+
+
+			isConected = qu.isConnected (2, 3);
+			Console.WriteLine ("2 and 3 " + isConected);
+
+			qu.Union (3, 5);
+
+		    isConected = qu.isConnected (3, 5);
+
+			Console.WriteLine ("3 and 5 " + isConected);
+
+			isConected = qu.isConnected (2, 5);
+
+			Console.WriteLine ("2 and 5 " + isConected);
+
+
+		}
+	}
+}
---
+++ QuickUnion/QuickUnion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace QuickUnion
+{
+	public class QuickUnion
+	{
+		int[] ID;
+
+		public QuickUnion (int N)
+		{
+			ID = new int[N];
+
+			for (int i = 0; i < N; i++) {
+
+				ID [i] = i;
+			}
+		}
+
+		public int getRoot(int p)
+		{
+			while (ID [p] != p)
+				p = ID [p];
+
+			return p;
+		}
+
+		public bool isConnected(int p, int q)
+		{
+			return getRoot (p) == getRoot (q);
+		}
+
+		public void Union(int p, int q)
+		{
+			int rootP = getRoot (p);
+			int rootQ = getRoot (q);
+
+			ID [rootP] = rootQ;
+		}
+
+	}
+}
+
